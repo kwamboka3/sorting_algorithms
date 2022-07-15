44f85d1878089591e6d19a8216e15edb34326540
kwamboka3 <pkwamboka@flag42.com>
2022-07-16 00:47:26 +0300

    update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,25 +12,25 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int num = 1, tmp = 0;
-	size_t j;
+	int flag = 1, tmp = 0;
+	size_t i;
 
 	if (size < 2 || !array)
 		return;
 
-	while (num == 1)
+	while (flag == 1)
 	{
-		num = 0;
+		flag = 0;
 
-		for (j = 0; j < size - 1; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
 				print_array(array, size);
-				num = 1;
+				flag = 1;
 			}
 		}
 	}
