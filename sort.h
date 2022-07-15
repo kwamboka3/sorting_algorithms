413e66da4a46129e1c24de4187f0aa849657c695
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-07-16 00:55:54 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -27,4 +27,12 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+/*---- Our helper functions ----*/
+int checklist(listint_t **list, listint_t **current);
+void checkend(listint_t **current, listint_t **tmp, listint_t *list);
+int checkbeg(listint_t **current, listint_t **tmp,
+	     listint_t **hold, listint_t **list);
+int checkjuice(listint_t **current, listint_t **tmp,
+	       listint_t *list);
 #endif
