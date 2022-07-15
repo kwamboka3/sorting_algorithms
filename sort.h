1d945ea2e640c5a2ad4e232bbcb26718a2eedb9b
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-07-16 01:26:22 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -27,6 +27,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 /*---- Our helper functions ----*/
 int checklist(listint_t **list, listint_t **current);
@@ -35,4 +37,9 @@ int checkbeg(listint_t **current, listint_t **tmp,
 	     listint_t **hold, listint_t **list);
 int checkjuice(listint_t **current, listint_t **tmp,
 	       listint_t *list);
+int partition(int *array, size_t start, size_t end, size_t size);
+void realqs(int *array, int start, int end, size_t size);
+void split(int *tmp_arr, size_t left, size_t right, int *array);
+void combine(int *array, size_t left, size_t middle,
+	     size_t right, int *tmp_arr);
 #endif
