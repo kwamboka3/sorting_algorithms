3c1e6d64c0ba629505e9d4cb158df933f69bd63a
kwamboka3 <pkwamboka@flag42.com>
2022-07-20 10:45:48 +0300

    sort.h

--- sort.h
+++ sort.h
@@ -38,4 +38,17 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
+/*---- Our helper functions ----*/
+int checklist(listint_t **list, listint_t **current);
+void checkend(listint_t **current, listint_t **tmp, listint_t *list);
+int checkbeg(listint_t **current, listint_t **tmp,
+	     listint_t **hold, listint_t **list);
+int checkjuice(listint_t **current, listint_t **tmp,
+	       listint_t *list);
+int partition(int *array, size_t start, size_t end, size_t size);
+void realqs(int *array, int start, int end, size_t size);
+void split(int *tmp_arr, size_t left, size_t right, int *array);
+void combine(int *array, size_t left, size_t middle,
+	     size_t right, int *tmp_arr);
+
 #endif
